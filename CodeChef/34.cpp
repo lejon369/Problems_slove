d8d745c1de81d593f1a88acd74cd1830cec6314c
Lejon <134403855+lejon369@users.noreply.github.com>
2025-09-24 21:32:53 +0600

    Add files via upload

---
+++ CodeChef/34.cpp
@@ -0,0 +1,20 @@
+//Update the code below to solve the problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X, Y, tuition;
+        cin >> X >> Y;
+        // Add some code here.
+        tuition = X * Y;
+        cout << tuition << endl;
+
+    }
+    return 0;
+}
---
+++ CodeChef/35.cpp
@@ -0,0 +1,20 @@
+//Update the code below to solve the problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int X, Y=10,mul;
+            cin >> X;
+            mul=X*Y;
+            cout << mul << endl;        
+        }
+
+
+return 0;
+        }
