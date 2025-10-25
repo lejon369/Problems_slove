ed33788b77d782ab09d807b75544ad0ae3f6aede
Lejon <134403855+lejon369@users.noreply.github.com>
2025-10-25 21:22:34 +0600

    Add files via upload

---
+++ CodeChef/56.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y,clear;
+    cin>>x>>y;
+    clear =7-(x+y);
+    cout<<clear<<endl;
+    return 0;
+}
